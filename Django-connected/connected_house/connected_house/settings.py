66ca3fbe19688c3746eadfb1d0179f4aa3fa63d5
Thomas-Dot-G <thomas.garesse@gmail.com>
2016-01-31 17:02:56 +0100

    #37 index static html

--- Django-connected/connected_house/connected_house/settings.py
+++ Django-connected/connected_house/connected_house/settings.py
@@ -85,11 +85,11 @@ DATABASES = {
 PROJECT_DIR  = os.path.dirname(__file__) 
 MEDIA_ROOT = os.path.join(PROJECT_DIR,'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(PROJECT_DIR,'static')
+#STATIC_ROOT = os.path.join(PROJECT_DIR,'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'staticfiles'),
+    os.path.join(PROJECT_DIR, 'static/'),
 )
 
 # Password validation
