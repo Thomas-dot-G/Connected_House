7d493cb63de875c96bfbcfeead16e996ae5b4adf
KingKemar <kingkemarlamalice@yahoo.fr>
2016-02-02 10:00:02 +0000

    django models updated

--- Django-connected/connected_house/connected_house/settings.py
+++ Django-connected/connected_house/connected_house/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'timezone_field',
+    'website.apps.WebsiteConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
