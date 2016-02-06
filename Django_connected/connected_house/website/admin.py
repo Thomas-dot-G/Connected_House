4d15c85472b62e148461e2dc8efc255e123aafdb
Thomas-Dot-G <thomas.garesse@gmail.com>
2016-02-06 19:52:13 +0100

    #40 add database

--- Django_connected/connected_house/website/admin.py
+++ Django_connected/connected_house/website/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from website.models import User,Channel,Sensor,Bridge
+from website.models import User,Channel,Sensor,Bridge,Data,Version
 
 
 admin.site.register(User)
 admin.site.register(Channel)
 admin.site.register(Sensor)
 admin.site.register(Bridge)
+admin.site.register(Data)
+admin.site.register(Version)
