df2a4124b773c94c134ee6f93357d56ff9126840
Thomas-Dot-G <thomas.garesse@gmail.com>
2016-02-16 21:45:15 +0100

    improved forecast

--- Django_connected/connected_house/server/static/js/ForecastCtrl.js
+++ Django_connected/connected_house/server/static/js/ForecastCtrl.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
 		}
 		else {
 			var city = localStorage.getItem('forecastcity');
+			$( ".CityName" ).replaceWith("<div class='CityName'>"+city+"</div>");
 			var days = [];
 			var temp = [];
 			var image = [];
@@ -62,7 +63,6 @@ $(document).ready(function () {
 					for(var i=0; i<image.length; i++){
 						htmlimg = htmlimg+'<td class="image"><img src="../static/images/weather/'+image[i]+'.png"></td>';
 					}
-					console.log(htmlimg);
 					$( ".image" ).replaceWith(htmlimg);
 
 					var htmlwind = "";
@@ -97,6 +97,7 @@ $(document).ready(function () {
 		var wind = [];
 		var city = document.getElementById("city").value;
 		console.log(city);
+		console.log('submitcityargtaken');
 		$(".ready").hide();
 		$(".error").hide();
 		var xmlHttp = new XMLHttpRequest();
@@ -105,9 +106,9 @@ $(document).ready(function () {
 	//$http.get('http://api.openweathermap.org/data/2.5/forecast/daily?q='+$scope.city+',FR&mode=json&units=metric&cnt=7&APPID='+APIKEYOPENWEATHER)
 	//.then(function(result) {
 				var result = JSON.parse(xmlHttp.responseText);
-				console.log(result);
 				var list = result.list;
 				var city = result.city.name;
+				$( ".CityName" ).replaceWith("<div class='CityName'>"+city+"</div>");
 				list.forEach(function(element){
 					var dateObject = new Date(element.dt * 1000);
 					var dateReadable = jours[dateObject.getDay()]+' '+dateObject.getDate();
@@ -117,6 +118,38 @@ $(document).ready(function () {
 					weather.push(element.weather[0].description);
 					wind.push(Math.floor(element.speed));
 				});
+				var htmldays = "";
+					for(var i=0; i<days.length; i++){
+						htmldays = htmldays+'<th class="days">'+days[i]+'</th>';
+
+					}
+					$( ".days" ).replaceWith(htmldays);
+
+					var htmltemp = "";
+					for(var i=0; i<temp.length; i++){
+						htmltemp = htmltemp+'<td class="temperature">'+temp[i].morn+'°C | '+temp[i].eve+'°C</td>';
+
+					}
+					$( ".temperature" ).replaceWith(htmltemp);
+
+					var htmlweather = "";
+					for(var i=0; i<weather.length; i++){
+						htmlweather = htmlweather+'<td class="weather">'+weather[i]+'</td>';
+
+					}
+					$( ".weather" ).replaceWith(htmlweather);
+
+					var htmlimg = "";
+					for(var i=0; i<image.length; i++){
+						htmlimg = htmlimg+'<td class="image"><img src="../static/images/weather/'+image[i]+'.png"></td>';
+					}
+					$( ".image" ).replaceWith(htmlimg);
+
+					var htmlwind = "";
+					for(var i=0; i<wind.length; i++){
+						htmlwind = htmlwind+'<td>'+wind[i]+' km/h</td>';
+					}
+					$( ".wind" ).replaceWith(htmlwind);
 				var tomorrow = new Date();
 				tomorrow.setDate(tomorrow.getDate() + 2);
 				/*var a = new Date();
@@ -126,9 +159,10 @@ $(document).ready(function () {
 				localStorage.setItem('forecastcity', city);
 				localStorage.setItem('forecastexp', tomorrow);
 				$(".ready").show();
+				$(".error").hide();
 			}
 			else {
-				$( ".cityname" ).replaceWith('<strong class="cityname"> '+ city +'</strong>');
+				$( ".city_name" ).replaceWith('<strong class="city_name"> '+ city +'</strong>');
 				$(".error").show();
 			}
 		};
